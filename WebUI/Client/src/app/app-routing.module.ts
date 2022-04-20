34a06383ce86354ec6d1bb316173e9337085d564
Krishna Bogati <krishna.bogati@imark.com.np>
2022-04-20 14:44:44 +0545

    Added Employee component on frontend side

--- WebUI/Client/src/app/app-routing.module.ts
+++ WebUI/Client/src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EmployeeAddComponent } from './Employees/components/employee-add/employee-add.component';
+import { EmployeeViewComponent } from './Employees/components/employee-view/employee-view.component';
 
-const routes: Routes = [];
+const appRoutingConstants = [
+  { path: '', redirectTo: 'ViewEmployee', pathMatch: "full" },
+  { path: 'ViewEmployee', component: EmployeeViewComponent },
+  { path: 'AddEmployee', component: EmployeeAddComponent },
+]
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(appRoutingConstants)],
   exports: [RouterModule]
 })
+
+
 export class AppRoutingModule { }
--- WebUI/Client/src/app/app.module.ts
+++ WebUI/Client/src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from "@angular/common/http";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EmployeeAddComponent } from './Employees/components/employee-add/employee-add.component';
+import { EmployeeViewComponent } from './Employees/components/employee-view/employee-view.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmployeeAddComponent,
+    EmployeeViewComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
